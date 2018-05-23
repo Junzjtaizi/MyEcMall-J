43d77f6837671ddd970983681b3d3279dd908dc6
NieKing Liu <jzjtaizi@qq.com>
2018-05-23 16:39:55 +0800

    Completed Framework.

--- MyEcMall-J/example/src/main/java/cn/nieking/myecmallj/ExampleApp.java
+++ MyEcMall-J/example/src/main/java/cn/nieking/myecmallj/ExampleApp.java
@@ -2,7 +2,10 @@ package cn.nieking.myecmallj;
 
 import android.app.Application;
 
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
 import cn.nieking.latte.core.app.Latte;
+import cn.nieking.latte.ec.icon.FontEcModule;
 
 public class ExampleApp extends Application {
 
@@ -10,6 +13,8 @@ public class ExampleApp extends Application {
     public void onCreate() {
         super.onCreate();
         Latte.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withIcon(new FontEcModule())
                 .withApiHost("http://127.0.0.1/")
                 .configure();
     }
